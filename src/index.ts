50e8c7fa0c3651279333e553d89301838d4623c0
Teddy Arida-Moody <t-teddya@microsoft.com>
2025-07-29 14:18:01 -0700

    added prompt.ts, restructured things

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import { ChatPrompt } from '@microsoft/teams.ai';
 import { App } from '@microsoft/teams.apps';
-import { ConsoleLogger } from '@microsoft/teams.common/logging';
 import { DevtoolsPlugin } from '@microsoft/teams.dev';
-import { McpClientPlugin } from '@microsoft/teams.mcpclient';
-import { OpenAIChatModel } from '@microsoft/teams.openai';
+import { createMCPAgent } from './prompt';
 
-const logger = new ConsoleLogger('mcp-client', { level: 'debug' });
-
-const prompt = new ChatPrompt({
-  model: new OpenAIChatModel({
-    model: process.env.AOAI_MODEL!,
-    apiKey: process.env.AOAI_API_KEY!,
-    endpoint: process.env.AOAI_ENDPOINT!,
-    apiVersion: '2025-04-01-preview'
-  },
-  ), logger,
-}, [new McpClientPlugin({ logger })],
-)
-  .usePlugin('mcpClient', {
-    url: 'https://learn.microsoft.com/api/mcp',
-  });
+const prompt = createMCPAgent('Microsoft Learn', 'https://learn.microsoft.com/api/mcp');
 
 const app = new App({
   plugins: [
---
+++ src/prompt.ts
@@ -0,0 +1,29 @@
+import { ChatPrompt } from '@microsoft/teams.ai';
+import { McpClientPlugin } from '@microsoft/teams.mcpclient';
+import { OpenAIChatModel } from '@microsoft/teams.openai';
+import { ConsoleLogger } from '@microsoft/teams.common/logging';
+
+const logger = new ConsoleLogger('mcp-client', { level: 'debug' });
+
+const generatePromptInstructions = (mcpServerName: string) => { 
+  return `You are a MCP Agent that is tied to the ${mcpServerName} MCP server. Your main purpose is to call on tools available from the MCP server. For ANY query related to ${mcpServerName}, use the tools to the best of your ability to answer.`
+}
+
+export const createMCPAgent = (mcpServerName: string, mcpServerLink: string) => {
+  const prompt = new ChatPrompt({
+    instructions: generatePromptInstructions(mcpServerName),
+    model: new OpenAIChatModel({
+      model: process.env.AOAI_MODEL!,
+      apiKey: process.env.AOAI_API_KEY!,
+      endpoint: process.env.AOAI_ENDPOINT!,
+      apiVersion: '2025-04-01-preview'
+    },
+    ), logger,
+  }, [new McpClientPlugin({ logger })],
+  )
+    .usePlugin('mcpClient', {
+      url: mcpServerLink,
+    });
+
+  return prompt;
+}
